3dded81d032eaf72e1eec708c9646c3cff6688d3
Varsha71 <47056517+Varsha71@users.noreply.github.com>
2021-01-29 15:08:42 +0530

    resources

---
+++ resources/static/index.js
@@ -0,0 +1,19 @@
+document.getElementById("Programming").addEventListener("click",function(){
+	document.getElementById("cate").value= this.id;
+	document.getElementById("frm").submit();
+});
+
+document.getElementById("Pun").addEventListener("click",function(){
+	document.getElementById("cate").value= this.id;
+	document.getElementById("frm").submit();
+});
+
+document.getElementById("Spooky").addEventListener("click",function(){
+	document.getElementById("cate").value= this.id;
+	document.getElementById("frm").submit();
+});
+
+document.getElementById("Christmas").addEventListener("click",function(){
+	document.getElementById("cate").value= this.id;
+	document.getElementById("frm").submit();
+});
